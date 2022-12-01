695c2509538df1fb3bab3ac98472e55f4d3ce2e2
오진수 <ohjinsu@codersproduct.com>
2022-12-01 18:51:16 +0900

    Refactor

--- src/lambdas/api_gateway_proxy.ts
+++ src/lambdas/api_gateway_proxy.ts
@@ -1,5 +1,6 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
 import { lambda, Lambda } from "../core/lambda";
+import { UseCase } from "../core/usecase";
 import { apiGatewayProxyRequestMapper } from "../request_mappers";
 import { ApiGatewayProxyResponse, apiGatewayProxyResponseMapper } from "../response_mappers";
 
@@ -8,8 +9,6 @@ import { ApiGatewayProxyResponse, apiGatewayProxyResponseMapper } from "../respo
  */
 export type ApiGatewayProxyLambda<Req, Res> = Lambda<APIGatewayProxyHandler, Req, Res>;
 
-export function apiGatewayProxyLambda<Req, Res extends ApiGatewayProxyResponse>(
-    usecase: Parameters<ApiGatewayProxyLambda<Req, Res>>[1],
-): ReturnType<ApiGatewayProxyLambda<Req, Res>> {
+export function apiGatewayProxyLambda<Req, Res extends ApiGatewayProxyResponse>(usecase: UseCase<Req, Res>): ReturnType<ApiGatewayProxyLambda<Req, Res>> {
     return lambda(apiGatewayProxyRequestMapper, usecase, apiGatewayProxyResponseMapper);
 }
